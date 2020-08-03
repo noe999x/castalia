574f0d887e3435b8dfe3288ccbc10d829ae525a4
CastaliaIkz <68614343+Kagurazaga@users.noreply.github.com>
2020-08-03 20:19:55 +0000

    Update ikz.py

--- ikz.py
+++ ikz.py
@@ -25,6 +25,16 @@ else:
 print("\033[1;33m!!! PERINGATAN !!!\033[0m\n")
 #Clear
 #end
+def jalan (kata):
+ for e in kata:
+  sys.stdout.write(e)
+  sys.stdout.flush()
+  time.sleep(0.1)
+jalan('\033[1;91mBuat lu para decoder script, gua peringatkan\033[0m\n'.center(44))
+jalan('\033[1;91mKalo lu berani nyolong script gua tanpa seijin gua\033[0m\n'.center(44))
+jalan('\033[1;91minget!. gua bakal datang ke rumah lu, gua tusuk lobang p****t lu\033[0m\n'.center(44))
+jalan('\033[1;91minget itu anjing, bikin SC ga gampang ya ppq\033[0m\n'.center(44))
+jalan('\033[1;32mBtw, Gue ganteng tq.!\033[0m\n'.center(44))
 
 id = []
 die = 0
